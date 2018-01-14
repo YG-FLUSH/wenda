67eeacb0946e31483385b1bcdb8d1f323bb8078f
YoGe <ygguang1992@gmail.com>
2018-01-14 13:35:30 +0800

    add config

---
+++ config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#   Author: Yoge
+#   Time: 2018/01/14
+
+
+config = {
+    'baidu': {
+        'app_id': "10688064",
+        'app_key': "361pYgGhVMte889c8Yex9Fqc",
+        "app_secret": "pythX2y3GQfHktrhQAuAsE6YvR3Ba1AU",
+    }
+}
